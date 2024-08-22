d42a2bf7fffc359c9ed5d75d262629aa05ee4787
aleksandramuraveva <84822235+aleksandramuraveva@users.noreply.github.com>
2024-08-22 21:19:07 +0300

    refactor: apply prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,28 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const WelcomePage: React.FC = () => {
   return (
     <main>
       <h1>Welcome</h1>
-        <nav>
+      <nav>
         <ul>
-          <li><Link href="/signin">Sign In</Link></li>
-          <li><Link href="/signup">Sign Up</Link></li>
+          <li>
+            <Link href="/signin">Sign In</Link>
+          </li>
+          <li>
+            <Link href="/signup">Sign Up</Link>
+          </li>
         </ul>
         <ul>
-          <li><Link href="/history">History</Link></li>
-          <li><Link href="/restclient">REST Client</Link></li>
-          <li><Link href="/graphiql">GraphiQL</Link></li>
+          <li>
+            <Link href="/history">History</Link>
+          </li>
+          <li>
+            <Link href="/restclient">REST Client</Link>
+          </li>
+          <li>
+            <Link href="/graphiql">GraphiQL</Link>
+          </li>
         </ul>
       </nav>
     </main>
