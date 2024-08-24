e4b9b83ec972b48905b456660017b96064926982
Olga <kartoshkaslukom@gmail.com>
2024-08-24 19:34:36 +0200

    feat: add url input and body input

--- src/components/GraphiQLClient/GraphiQLClient.tsx
+++ src/components/GraphiQLClient/GraphiQLClient.tsx
@@ -1,3 +1,41 @@
-const GraphQLClient: React.FC = () => <div>GraphQLClient</div>;
+import axios from "axios";
+import { useState } from "react";
+
+const GraphQLClient: React.FC = () => {
+  const [url, setUrl] = useState("");
+  const [body, setBody] = useState("");
+  return (
+    <div>
+      <label htmlFor="url">url</label>
+      <input
+        type="text"
+        id="url"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      ></input>
+      <label htmlFor="body">request</label>
+      <textarea
+        id="body"
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+      ></textarea>
+      <button onClick={() => fetchData(url, body)}>Send</button>
+    </div>
+  );
+};
+
+const fetchData = async (url, query) => {
+  const response = await axios.post(
+    url,
+    {
+      query,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+};
 
 export default GraphQLClient;
--- src/pages/graphiql.tsx
+++ src/pages/graphiql.tsx
@@ -1,7 +1,10 @@
+import GraphQLClient from "../components/GraphiQLClient/GraphiQLClient";
+
 const GraphiQLPage: React.FC = () => {
   return (
     <main>
       <h1>GraphiQL</h1>
+      <GraphQLClient></GraphQLClient>
     </main>
   );
 };
