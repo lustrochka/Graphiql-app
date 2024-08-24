a5d386de4ee5f601e75fb2407836b8680748ac49
Olga <kartoshkaslukom@gmail.com>
2024-08-24 20:44:29 +0200

    feat: add variables

--- src/components/GraphiQLClient/GraphiQLClient.tsx
+++ src/components/GraphiQLClient/GraphiQLClient.tsx
@@ -1,11 +1,24 @@
 import axios from "axios";
 import { useState } from "react";
+import styles from "./GraphiQLClient.module.css";
 
 const GraphQLClient: React.FC = () => {
-  const [url, setUrl] = useState("");
-  const [body, setBody] = useState("");
+  const DEFAULT_URL = "https://rickandmortyapi.com/graphql";
+  const DEFAULT_BODY = `
+    query GetCharacter($id: ID!) {
+      character(id: $id) {
+        name
+        species
+      }
+    }
+    `;
+  const DEFAULT_VARIABLES = `{"id": 1}`;
+  const [url, setUrl] = useState(DEFAULT_URL);
+  const [body, setBody] = useState(DEFAULT_BODY);
+  const [variables, setVariables] = useState(DEFAULT_VARIABLES);
+
   return (
-    <div>
+    <div className={styles.graphql}>
       <label htmlFor="url">url</label>
       <input
         type="text"
@@ -17,18 +30,28 @@ const GraphQLClient: React.FC = () => {
       <textarea
         id="body"
         value={body}
+        rows={10}
+        cols={33}
         onChange={(e) => setBody(e.target.value)}
       ></textarea>
-      <button onClick={() => fetchData(url, body)}>Send</button>
+      <label htmlFor="variables">variables</label>
+      <textarea
+        id="variables"
+        value={variables}
+        onChange={(e) => setVariables(e.target.value)}
+      ></textarea>
+      <button onClick={() => fetchData(url, body, variables)}>Send</button>
     </div>
   );
 };
 
-const fetchData = async (url, query) => {
+const fetchData = async (url, query, variables) => {
+  const parsedVars = JSON.parse(variables || "{}");
   const response = await axios.post(
     url,
     {
       query,
+      variables: parsedVars,
     },
     {
       headers: {
