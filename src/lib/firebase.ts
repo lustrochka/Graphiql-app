1f7322a5cd082c5b250f8bd989f69ac57c294c39
aleksandramuraveva <84822235+aleksandramuraveva@users.noreply.github.com>
2024-08-29 12:26:13 +0300

    feat: integrate firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5dQUSV2Cd3IT1vyq8Bf7JjSM1sbsP2Ho",
+  authDomain: "graphiql-app-rss-24.firebaseapp.com",
+  projectId: "graphiql-app-rss-24",
+  storageBucket: "graphiql-app-rss-24.appspot.com",
+  messagingSenderId: "890078368816",
+  appId: "1:890078368816:web:a3e7ec8037cc86bc4ac9f5"
+};
+
+
+const app = initializeApp(firebaseConfig);
