429f605e20ca219beee85fd88a5b49a0a628c90e
aleksandramuraveva <84822235+aleksandramuraveva@users.noreply.github.com>
2024-08-29 13:21:54 +0300

    refactor: finish firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,20 @@
 import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyA5dQUSV2Cd3IT1vyq8Bf7JjSM1sbsP2Ho",
@@ -9,4 +25,44 @@ const firebaseConfig = {
   appId: "1:890078368816:web:a3e7ec8037cc86bc4ac9f5",
 };
 
-const app = initializeApp(firebaseConfig);
+const app = ​​initializeApp(firebaseConfig);
+​​const auth = getAuth(app);
+​​const db = getFirestore(app);
+
+
+const logInWithEmailAndPassword = async (email: string, password: string) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const registerWithEmailAndPassword = async (name: string, email: string, password: string) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export {
+  auth,
+  db,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  logout,
+};
