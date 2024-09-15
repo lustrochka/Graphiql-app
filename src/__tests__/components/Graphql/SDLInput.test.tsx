21299b18a0404986c595e94dd01da96cac2366ee
Olga <kartoshkaslukom@gmail.com>
2024-09-15 10:49:50 +0200

    feat: add sdl test

---
+++ src/__tests__/components/Graphql/SDLInput.test.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { FormProvider, useForm, useFormContext } from "react-hook-form";
+import SDLInput from "../../../components/GraphiQLClient/SdlInput";
+import { useEffect } from "react";
+
+const url = "https://url.com";
+
+const Form: React.FC<{ value: string }> = ({ value }) => {
+  const methods = useForm();
+  const { setValue } = methods;
+
+  useEffect(() => {
+    setValue("url", value);
+  }, []);
+
+  return (
+    <FormProvider {...methods}>
+      <SDLInput />
+    </FormProvider>
+  );
+};
+
+test("updates sdl value when url value changing", async () => {
+  render(<Form value={url} />);
+
+  const sdlInput = screen.getByRole("textbox") as HTMLInputElement;
+
+  expect(sdlInput.value).toBe(`${url}?sdl`);
+});
+
+test("correctly handles empty url value", async () => {
+  render(<Form value="" />);
+
+  const sdlInput = screen.getByRole("textbox") as HTMLInputElement;
+
+  expect(sdlInput.value).toBe("");
+});
